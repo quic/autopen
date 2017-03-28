747994b8dab72b372206da000df858f5df8b4a89
velvetbleurose <jguerero@bu.edu>
2017-03-28 16:15:38 -0400

    implemented basic button flow

--- GUI/other_main.py
+++ GUI/other_main.py
@@ -16,6 +16,7 @@ from kivy.uix.label import Label
 
 from kivy.lang import Builder
 
+#from tools import test_install
 
 Builder.load_string('''
 <Container>:
@@ -64,6 +65,10 @@ class AutoPen(App):
 			self.miscellaneous = Button(text='Miscellaneous', size_hint=(.2,.1), pos_hint={'x':.7, 'y':.6})
 			self.back = Button(text='Back', size_hint=(.1,.05), pos_hint={'x':0, 'y':0})
 
+
+			tools_title = Button(text='Tools', size_hint=(.6,.3), pos_hint={'x':.2,'y':.8}, background_color=[1,0,1,0])
+			self.mainpage.add_widget(tools_title)
+
 			#adding buttons to main tools
 			self.mainpage.add_widget(self.can)
 			self.mainpage.add_widget(self.wifi_SDR)
@@ -80,9 +85,11 @@ class AutoPen(App):
 		def databasePage(instance):
 			self.mainpage.remove_widget(self.welcome)
 			self.mainpage.remove_widget(self.database)
+			self.mainpage.remove_widget(self.howto)
+			self.mainpage.remove_widget(self.terms)
 			self.mainpage.remove_widget(self.tools)
 
-			database_title = Button(text='Database', size_hint=(.6,.3), pos_hint={'x':.2,'y':.7}, background_color=[1,0,0,1])
+			database_title = Button(text='Database', size_hint=(.6,.3), pos_hint={'x':.2,'y':.7}, background_color=[1,0,0,0])
 			self.mainpage.add_widget(database_title)
 
 			cont = Button(text='Continue', size_hint=(.1,.05), pos_hint={'x':.9, 'y':0})
@@ -92,23 +99,27 @@ class AutoPen(App):
 		self.welcome = Button(text='Welcome to Autopen', size_hint=(.6,.3), pos_hint={'x':.2,'y':.7}, background_color=[1,0,1,1])
 		self.mainpage.add_widget(self.welcome)
 
-		self.howto = Button(text='How-To', size_hint=(.3,.1), pos_hint={'x':.3, 'y':.4})
-		self.tools = Button(text='Search', size_hint=(.3,.1), pos_hint={'x':.3, 'y':.3})
-		self.terms = Button(text='Terms and Conditions', size_hint=(.3,.1), pos_hint={'x':.3, 'y':.2})
+		self.howto = Button(text='How-To', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.4})
+		self.tools = Button(text='Search', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.3})
+		self.terms = Button(text='Terms and Conditions', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.2})
 
 		self.mainpage.add_widget(self.howto)
 		self.mainpage.add_widget(self.tools)
 		self.mainpage.add_widget(self.terms)
 
-		self.database = Button(text='Register Vehicle', size_hint=(.3,.1), pos_hint={'x':.3, 'y':.6})
+		self.database = Button(text='Register Vehicle', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.6})
 		self.mainpage.add_widget(self.database)
 		self.database.bind(on_press=databasePage)
 
-		self.tools = Button(text='Tools', size_hint=(.3,.1), pos_hint={'x':.3, 'y':.5})
+		self.tools = Button(text='Tools', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.5})
 		self.mainpage.add_widget(self.tools)
 		self.tools.bind(on_press=MainPage)
 
+		def canutilsPage(instance):
 
+			self.install = Button(text='Install', size_hint=(.2,.1), pos_hint={'x':.4, 'y':.1})
+			self.mainpage.add_widget(self.install)
+			#self.install.bind(on_press=test_install.test())
 
 		def canPage(instance):
 			self.mainpage.remove_widget(self.can)
@@ -123,7 +134,7 @@ class AutoPen(App):
 			can_title = Button(text='CAN Tools', size_hint=(.2,.1), pos_hint={'x':.05,'y':.85}, background_color=[1,0,0,0.5])
 			self.mainpage.add_widget(can_title)
 
-			canutils = Button(text='Can-utils', size_hint=(.2,.1), pos_hint={'x':.05,'y':.75})
+			self.canutils = Button(text='Can-utils', size_hint=(.2,.1), pos_hint={'x':.05,'y':.75})
 			canbus_utils = Button(text='Canbus-utils', size_hint=(.2,.1), pos_hint={'x':.05,'y':.65})
 			kayak = Button(text='Kayak', size_hint=(.2,.1), pos_hint={'x':.05, 'y':.55})
 			caringcaribou = Button(text='Caring Caribou', size_hint=(.2,.1), pos_hint={'x':.05, 'y':.45})
@@ -132,9 +143,7 @@ class AutoPen(App):
 			DO = Button(text='PyOBD',size_hint=(.2,.1), pos_hint={'x':.05,'y':.15})
 			O2OO = Button(text='O2OO', size_hint=(.2,.1), pos_hint={'x':.05, 'y':.05})
 
-
-
-			self.mainpage.add_widget(canutils)
+			self.mainpage.add_widget(self.canutils)
 			self.mainpage.add_widget(canbus_utils)
 			self.mainpage.add_widget(c0f)
 			self.mainpage.add_widget(kayak)
@@ -144,6 +153,7 @@ class AutoPen(App):
 			self.mainpage.add_widget(O2OO)
 
 			self.back.bind(on_press=MainPage)
+			self.canutils.bind(on_press=canutilsPage)
 
 		def bluetoothPage(instance):
 			self.mainpage.remove_widget(self.can)
@@ -154,7 +164,7 @@ class AutoPen(App):
 			tools_title = Button(text='Tools', size_hint=(.6,.3), pos_hint={'x':.2,'y':.8}, background_color=[1,0,1,0])
 			self.mainpage.add_widget(tools_title)
 
-			bluetooth_title = Button(text='Bluetooth Tools', size_hint=(.25,.1), pos_hint={'x':.05,'y':.85}, background_color=[1,0,1,0])
+			bluetooth_title = Button(text='Bluetooth Tools', size_hint=(.25,.1), pos_hint={'x':.05,'y':.85}, background_color=[1,0,0,0.5])
 			self.mainpage.add_widget(bluetooth_title)
 
 			bluelog = Button(text='Bluelog', size_hint=(.25,.1), pos_hint={'x':.05,'y':.75})
@@ -162,10 +172,14 @@ class AutoPen(App):
 			btscanner = Button(text='BTScanner', size_hint=(.25,.1), pos_hint={'x':.05, 'y':.55})
 			bluetooth_tools = Button(text='Bluetooth Tools Package', size_hint=(.25,.1), pos_hint={'x':.05, 'y':.45})
 
+			install = Button(text='Install', size_hint=(.2,.1), pos_hint={'x':.4, 'y':.1})
+
 			self.mainpage.add_widget(bluelog)
 			self.mainpage.add_widget(bluemaho)
 			self.mainpage.add_widget(btscanner)
 			self.mainpage.add_widget(bluetooth_tools)
+			self.mainpage.add_widget(install)
+
 
 			self.back.bind(on_press=MainPage)
 
@@ -178,14 +192,18 @@ class AutoPen(App):
 			tools_title = Button(text='Tools', size_hint=(.6,.3), pos_hint={'x':.2,'y':.8}, background_color=[1,0,1,0])
 			self.mainpage.add_widget(tools_title)
 
-			title = Button(text='Wi-fi/SDR Tools', size_hint=(.25,.1), pos_hint={'x':.05,'y':.85}, background_color=[1,0,1,0])
+			title = Button(text='Wi-fi/SDR Tools', size_hint=(.25,.1), pos_hint={'x':.05,'y':.85}, background_color=[1,0,0,0.5])
 			self.mainpage.add_widget(title)
 
 			aircrack = Button(text='Aircrack-ng', size_hint=(.25,.1), pos_hint={'x':.05,'y':.75})
 			gnuradio = Button(text='GNU Radio', size_hint=(.25,.1), pos_hint={'x':.05,'y':.65})
 
+			install = Button(text='Install', size_hint=(.2,.1), pos_hint={'x':.4, 'y':.1})
+
 			self.mainpage.add_widget(aircrack)
 			self.mainpage.add_widget(gnuradio)
+			self.mainpage.add_widget(install)
+
 
 			self.back.bind(on_press=MainPage)
 
@@ -198,14 +216,17 @@ class AutoPen(App):
 			tools_title = Button(text='Tools', size_hint=(.6,.3), pos_hint={'x':.2,'y':.8}, background_color=[1,0,1,0])
 			self.mainpage.add_widget(tools_title)
 
-			miscel_title = Button(text='Miscellaneous Tools', size_hint=(.25,.1), pos_hint={'x':.05,'y':.85}, background_color=[1,0,1,0])
+			miscel_title = Button(text='Miscellaneous Tools', size_hint=(.25,.1), pos_hint={'x':.05,'y':.85}, background_color=[1,0,0,0.5])
 			self.mainpage.add_widget(miscel_title)
 
 			katoolin = Button(text='Katoolin', size_hint=(.25,.1), pos_hint={'x':.05,'y':.75})
 			romraider = Button(text='Romraider', size_hint=(.25,.1), pos_hint={'x':.05,'y':.65})
 
+			install = Button(text='Install', size_hint=(.2,.1), pos_hint={'x':.4, 'y':.1})
+
 			self.mainpage.add_widget(katoolin)
 			self.mainpage.add_widget(romraider)
+			self.mainpage.add_widget(install)
 
 			self.back.bind(on_press=MainPage)
 
