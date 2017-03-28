ac4b930a428c10b14ad458b1efca8b57e0a080a6
velvetbleurose <jguerero@bu.edu>
2017-03-28 00:23:32 -0400

    added main tools page buttons and binded with listview

--- GUI/main.py
+++ GUI/main.py
@@ -40,10 +40,30 @@ class OtherGoodies():
 class AutoPen(App):
     def build(self):
 
-        btn = Button(text='Back', on_release=self.callback, size_hint=(0.15, 0.1), pos_hint={'x': 0, 'y': .9})
+        #btn = Button(text='Back', on_release=self.callback, size_hint=(0.15, 0.1), pos_hint={'x': 0, 'y': .9})
         car = Image(source='AutoPen.png')
 
         self.layout = FloatLayout()
+
+        #main tools buttons
+        can = Button(text='CAN', size_hint=(.2, .1), pos_hint={'x':.1, 'y':.2})
+        wifi = Button(text='Wi-fi', size_hint=(.2, .1), pos_hint = {'x':.1, 'y':.6})
+        bluetooth = Button(text='Bluetooth', size_hint=(.2,.1), pos_hint={'x':.7, 'y':.2})
+        SDR = Button(text='SDR', size_hint=(.2,.1), pos_hint={'x':.7, 'y':.6})
+
+        #adding buttons to main tools
+        self.layout.add_widget(can)
+        self.layout.add_widget(wifi)
+        self.layout.add_widget(bluetooth)
+        self.layout.add_widget(SDR)
+
+        #binding buttons to lists
+        can.bind(on_press=callback)
+        wifi.bind(on_press=callback)
+        bluetooth.bind(on_press=callback)
+        SDR.bind(on_press=callback)
+
+        #ListView parameters
         data = [{'text': 'CAN Tools', 'is_selected': False},{'text': 'Wi-Fi Tools', 'is_selected': False},{'text': 'Bluetooth Tools', 'is_selected': False},{'text': 'Miscellaneous', 'is_selected': False}]
         args_converter = lambda row_index, rec: {'text': rec['text'],
                                                  'size_hint_y': None,
@@ -55,15 +75,29 @@ class AutoPen(App):
                                    allow_empty_selection=True)
         list_view = ListView(adapter=list_adapter, size_hint=(0.2,0.9))
 
-        self.layout.add_widget(btn)
+        #self.layout.add_widget(btn)
         self.layout.add_widget(list_view)
         return self.layout
 
+
     def callback(self, value):
-        pass
-        #self.sub_list_view.adapter.data = [{'text': 'canutils', 'is_selected': False}, {'text': 'c0f', 'is_selected': False}, {'text': 'Kayak', 'is_selected': False}, {'text': 'UDSim', 'is_selected': False}]
-        #self.layout.do_layout()
+
+        if value == 'can':
+            self.sub_list_view.adapter.data = [{'text': 'canutils', 'is_selected': False}, {'text': 'c0f', 'is_selected': False}, {'text': 'Kayak', 'is_selected': False}, {'text': 'UDSim', 'is_selected': False}]
+        elif value == 'wifi':
+            self.sub_list_view.adapter.data = [{'text': 'aircrack-ng', 'is_selected': False}]
+        elif value == 'bluetooth':
+            self.sub_list_view.adapter.data = [{'text': 'bluemaho', 'is_selected': False}, {'text': 'btscanner', 'is_selected': False}]
+        elif value == 'SDR':
+            self.sub_list_view.adapter.data = [{'text': 'gnu-radio', 'is_selected': False}]
+
+        self.layout.do_layout()
 
 
 if __name__ == "__main__":
     AutoPen().run()
+
+
+
+
+
