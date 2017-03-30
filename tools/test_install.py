740c66fa95a469d4e88b6c10dbc3450323241f6b
velvetbleurose <jguerero@bu.edu>
2017-03-29 21:06:43 -0400

    removed todo comments

--- tools/test_install.py
+++ tools/test_install.py
@@ -3,21 +3,10 @@ import install_basics
 import dependencies
 import tools
 import subprocess
-'''
-TO DO:
 
-1. need to redirect all output to a log file 
-	possibly standard out with everything
-	standard error with just errors
-2. want to keep track of what's been installed and what hasn't to change buttons accordingly
-	when installation is done > change button to say open
-	also make a quick list to see what's installed and what hasn't been
-3. create a open.py for after things are installed and user wants to open the program
-
-'''
 def test():
-	#this will be placed somewhere else, maybe a dictionary?
-	repo_can_utils = 'https://github.com/linux-can/can-utils.git' #needed to run can-utils
+	#this might be placed somewhere else
+	repo_can_utils = 'https://github.com/linux-can/can-utils.git'
 	repo_canbus_utils = 'https://github.com/digitalbond/canbus-utils.git'
 	repo_kayak = 'https://github.com/dschanoeh/Kayak.git'
 	repo_caringcaribou = 'https://github.com/CaringCaribou/caringcaribou.git' #want to check this to make sure it works, instructions a bit unclear
@@ -30,7 +19,7 @@ def test():
 	repo_bluemaho = 'https://github.com/zenware/bluemaho.git'
 
 	#for download links
-	link_pyobd = 'http://www.obdtester.com/download/pyobd_0.9.3.tar.gz'
+	link_pyobd = 'http://www.obdtester.com/download/pyobd_0.9.3.tar.gz'	#this might not work
 	link_o2oo = 'https://www.vanheusden.com/O2OO/O2OO-0.9.tgz'
 	link_romraider = 'http://assembla.com/spaces/romraider/documents/a5Ao9gHEir5P9Udmr6QqzO/download/RomRaider0.5.9RC3-linux.jar'
 
@@ -55,24 +44,19 @@ def test():
 	#tools.installed_tools(pack_man, 'gnuradio')
 	#tools.installed_tools(pack_man, 'aircrack-ng')
 
+'''
 
-	'''
 
-	Things to note during testing/to fix: 
 
-	1. in order to run test_install.py , python3 has to be installed. So, we need to make it that they can just run the python version of make
-	2. if they clone from git, they have to have git installed. If they download zip, they do not (we will install for them). Need to include instructions for both 
-	3. in check_NPM, the version is printed out, might want to add print statements around that --> blank is installed with version number bleh
-	4. complete caringcaribou installation, need an actual device or ask omar to help set up a virtual 
-	5. fix all 'with error codes' to say 'error code'
-	6. fix in udsim tools, printing the correct library name
-	7. write open script
+
+
+
 
 	EXCEPTIONS TO CATCH:
 		1. if the repo has already been cloned, want to keep going
 		2. in dependencies in check_NPM (wrap command -v in exception catching FileNotFoundError), this is because if it's not there it throws this error
-		3. IF SOMETHING FAILS TO INSTALL. PASS AND SKIP THE REST !! 
-		4. If pip is not involved for some reason, need to be able to end installation
+		3. 
+		
 
 
 	ERRORS:
