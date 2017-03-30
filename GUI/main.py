e089c9ac072315f949b182579e8c85b0befed95d
velvetbleurose <jguerero@bu.edu>
2017-03-30 00:47:23 -0400

    added some database buttons

--- GUI/main.py
+++ GUI/main.py
@@ -11,33 +11,11 @@ from kivy.adapters.listadapter import ListAdapter
 from kivy.uix.listview import ListItemButton, ListView
 from kivy.base import runTouchApp
 from kivy.uix.spinner import Spinner
+from kivy.uix.textinput import TextInput
 
 from kivy.uix.label import Label
 
 from kivy.lang import Builder
-
-#from tools import test_install
-
-Builder.load_string('''
-<Container>:
-	FloatLayout:
-		canvas.before:
-			Rectangle:
-				source: 'background.jpg'
-				pos: self.pos
-				size: self.size
-
-<OtherGoodies>:
-	ListItemButton:
-		selected_color: 0.5, 0.5, 0.5, 1
-		deselected_color: 0.7, 0.7, 0.7, 1
-
-
-''')
-
-#:layout FloatLayout()
-
-
 
 class AutoPen(App):
 
@@ -54,7 +32,7 @@ class AutoPen(App):
 		self.mainpage = FloatLayout()
 
 		#btn = Button(text='Back', on_release=self.callback, size_hint=(0.15, 0.1), pos_hint={'x': 0, 'y': .9})
-		car = Image(source='AutoPen.png')
+		#car = Image(source='AutoPen.png')
 
 
 		def MainPage(instance):
@@ -88,23 +66,59 @@ class AutoPen(App):
 			self.mainpage.remove_widget(self.howto)
 			self.mainpage.remove_widget(self.terms)
 			self.mainpage.remove_widget(self.tools)
+			self.mainpage.remove_widget(self.search)
 
 			database_title = Button(text='Database', size_hint=(.6,.3), pos_hint={'x':.2,'y':.7}, background_color=[1,0,0,0])
 			self.mainpage.add_widget(database_title)
 
-			cont = Button(text='Continue', size_hint=(.1,.05), pos_hint={'x':.9, 'y':0})
-			self.mainpage.add_widget(cont)
-			cont.bind(on_press=MainPage)
+			self.create = Button(text='Create New Car', size_hint=(.2,.1), pos_hint={'x':.1, 'y':.5})
+			self.load = Button(text='Load Car', size_hint=(.2,.1), pos_hint={'x':.1,'y':.4})
+			self.back = Button(text='Back', size_hint=(.1,.05), pos_hint={'x':0, 'y':0})
+			self.cont = Button(text='Continue', size_hint=(.1,.05), pos_hint={'x':.9, 'y':0})
+
+
+			self.mainpage.add_widget(self.create)
+			self.mainpage.add_widget(self.load)
+			self.mainpage.add_widget(self.back)
+			self.mainpage.add_widget(self.cont)
+
+			#self.tools = Button(text='Tools', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.5})
+
+			self.create.bind(on_press=createCarButton)
+			self.load.bind(on_press=loadCarButton)
+			#self.back.bind(on_press=self.mainpage)
+			self.cont.bind(on_press=MainPage)
+
+		def on_text(instance, value):
+		    print('The widget', instance, 'have:', value)
+			
+
+		def createCarButton(button):
+			layout = button.parent
+			model = TextInput(text='Input Car Model', background_color=[1,1,0,0.5], size_hint=(.3,.1), pos_hint={'x':.3,'y':.5})
+			make = TextInput(text='Input Car Make', background_color=[1,1,0,0.5], size_hint=(.3,.1), pos_hint={'x':.3, 'y':.4})
+			year = TextInput(text='Input Car Year', background_color=[1,1,0,0.5], size_hint=(.3,.1), pos_hint={'x':.3, 'y':.3})
+			creat = Button(text='Create', size_hint=(.3,.1), pos_hint={'x':.3,'y':.2})
+
+			self.mainpage.add_widget(model)
+			self.mainpage.add_widget(make)
+			self.mainpage.add_widget(year)
+			self.mainpage.add_widget(creat)
+		#	print (textinput)
+
+
+		def loadCarButton(button):
+			layout = button.parent
 
 		self.welcome = Button(text='Welcome to Autopen', size_hint=(.6,.3), pos_hint={'x':.2,'y':.7}, background_color=[1,0,1,1])
 		self.mainpage.add_widget(self.welcome)
 
 		self.howto = Button(text='How-To', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.4})
-		self.tools = Button(text='Search', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.3})
+		self.search = Button(text='Search', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.3})
 		self.terms = Button(text='Terms and Conditions', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.2})
 
 		self.mainpage.add_widget(self.howto)
-		self.mainpage.add_widget(self.tools)
+		self.mainpage.add_widget(self.search)
 		self.mainpage.add_widget(self.terms)
 
 		self.database = Button(text='Register Vehicle', size_hint=(.3,.1), pos_hint={'x':.35, 'y':.6})
