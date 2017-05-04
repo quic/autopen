c0a7ef01baf987f4569226f283324739850d41d6
velvetbleurose <jguerero@bu.edu>
2017-05-04 19:46:15 -0400

    adding most recent code

--- test.py
+++ test.py
@@ -1,14 +1,33 @@
 import subprocess
 import os
 
-words = os.getcwd()
-back_index = words.rfind('/')
-name = words.find('autopen')
-if words[back_index:] != '/autopen':
-	print ('not in correct repo')
-	print (words[back_index:])
+
+
+
+
+# back_index = words.rfind('/')
+# name = words.find('autopen')
+# if words[back_index:] != '/autopen':
+# 	print ('not in correct repo')
+# 	print (words[back_index:])
+# else:
+# 	print (name)
+# 	print (words[:(name+7)])
+# 	print ('you right')
+# print (words)
+
+# f = open('output.txt', 'w')
+
+# master = subprocess.run(['git', 'rev-parse', 'master'], stdout=subprocess.PIPE)
+# print ((master.stdout).decode('utf-8'))
+# print ('UPDATE FAILED: failed to update tool')
+
+x = subprocess.run(['sudo', 'apt-get', 'install', 'wireshark']).returncode
+if x != 0:
+	print 'did not install'
 else:
-	print (name)
-	print (words[:(name+7)])
-	print ('you right')
-print (words)
+	y = subprocess.run(['sudo', 'apt-get', '--only-upgrade', 'install', 'wireshark']).returncode
+	if y != 0:
+		print 'dk whats going on'
+	else:
+		print 'hey it worked'
